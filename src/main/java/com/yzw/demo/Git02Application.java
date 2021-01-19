271878f2d3d73100df76bbbf2ddcb0b5bb13fb65
minorolech <279478472@qq.com>
2021-01-19 10:25:25 +0800

    初始化

---
+++ src/main/java/com/yzw/demo/Git02Application.java
@@ -0,0 +1,13 @@
+package com.yzw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzw/demo/Git02ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yzw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git02ApplicationTests {
+
+    @Test
+    public void testabc(){
+        System.out.println("abc.........");
+    }
+
+}
